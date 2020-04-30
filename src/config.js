d65f1613cf788b595770d8a7ace403e21260b220
Yvon-Medesse <yvon.medesse@gmail.com>
2020-04-30 12:30:10 +0100

    commit live

--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
 export const API = process.env.REACT_APP_API_URL;
-export const WOOCOMMERCE_URL = "https://oph.f19.myftpupload.com";
-// export const WOOCOMMERCE_URL = "https://www.librairielbu.com";
-// export const WOOCOMMERCE_KEY = "ck_f0635fd1accb5fdc8587e723de41d8fabb2ae350";
-// export const WOOCOMMERCE_SECRET = "cs_911ffdef5ddc5652be51ae70b5f990a8025e901b";
+export const WOOCOMMERCE_URL = "https://www.librairielbu.com";
+export const WOOCOMMERCE_KEY = "ck_f0635fd1accb5fdc8587e723de41d8fabb2ae350";
+export const WOOCOMMERCE_SECRET = "cs_911ffdef5ddc5652be51ae70b5f990a8025e901b";
 export const ADMIN_PRODUCT_IMAGE_SIZE = "150px";
 export const ADMIN_PRODUCT_MOBILE_IMAGE_SIZE = "80px";
 export const ADMIN_PRODUCT_VARIANT_IMAGE_SIZE = "80px";
 export const ADMIN_PRODUCT_VARIANT_MOBILE_IMAGE_SIZE = "80px";
 export const CATALOG_LOAD_LIMIT = 5;
-export const WOOCOMMERCE_KEY = "ck_a0b788e42b32f777fa86859fa9fbe95dd0bac179";
-export const WOOCOMMERCE_SECRET = "cs_0aa8f8c8fd00d72df43eabbf1408425780b9d5f9";
+
+// export const WOOCOMMERCE_URL = "https://oph.f19.myftpupload.com";
+// export const WOOCOMMERCE_KEY = "ck_a0b788e42b32f777fa86859fa9fbe95dd0bac179";
+// export const WOOCOMMERCE_SECRET = "cs_0aa8f8c8fd00d72df43eabbf1408425780b9d5f9";
